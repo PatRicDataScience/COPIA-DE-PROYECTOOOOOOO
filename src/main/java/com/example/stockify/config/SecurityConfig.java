7a3aba03166b53e2ec24e70a84474f4d162d0953
Nayeli <nayeli.guerrero@utec.edu.pe>
2025-11-15 06:03:59 -0500

    fix

--- src/main/java/com/example/stockify/config/SecurityConfig.java
+++ src/main/java/com/example/stockify/config/SecurityConfig.java
@@ -21,7 +21,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(securedEnabled = true)
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig {
 
     private final UsuarioService userDetailsService;
